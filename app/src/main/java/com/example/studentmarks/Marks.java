7e78d4bfc67787452938f194670216d5d7c7b12a
Egenue <gene23.chiki@gmail.com>
2025-06-02 19:07:39 +0300

    Marks page

--- app/src/main/java/com/example/studentmarks/Marks.java
+++ app/src/main/java/com/example/studentmarks/Marks.java
@@ -1,48 +1,34 @@
 package com.example.studentmarks;
 
 import static android.content.ContentValues.TAG;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class Marks extends AppCompatActivity {
-
-    EditText marks, studentName, subject,student_regNo, subject_code, cat, total;
-    Button btn_submit, view_all_marks;
+    TextView marks, studentName, subject, subject_code,student_regNo, cat, total;
+    Button btn_view_all;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marks);
 
-        view_all_marks.setOnClickListener(new View.OnClickListener() {
+        btn_view_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
+                connectingToFirebase(new Bean());
             }
         });
 
-        connectingToFirebase(new Bean());
     }
-
     private void connectingToFirebase(Bean bean) {
         // Get Firebase database reference
         FirebaseDatabase database = FirebaseDatabase.getInstance();
